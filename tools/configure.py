f2fcee2e07a4d449ca9006bfa5da67259786da8f
swient <leo0902265667@gmail.com>
2025-10-19 11:32:01 +0800

    feat: 初始完成大部分功能和調整部分設定 - 新增遊戲資源圖片與 pipeline 設定 - 完善 interface.json 及相關設定檔 - 配置 workflow、自動化與格式化工具 - 移動 agent 目錄至 assets/agent - 移動 CI 工具至 tools 目錄

--- tools/configure.py
+++ tools/configure.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import shutil
 
-assets_dir = Path(__file__).parent.resolve() / "assets"
+assets_dir = Path(__file__).parent.parent.resolve() / "assets"
 
 
 def configure_ocr_model():
@@ -11,8 +11,8 @@ def configure_ocr_model():
         print(f"File Not Found: {assets_ocr_dir}")
         exit(1)
 
-    ocr_dir = assets_dir / "resource" / "model" / "ocr"
-    if not ocr_dir.exists():   # copy default OCR model only if dir does not exist
+    ocr_dir = assets_dir / "resource" / "base" / "model" / "ocr"
+    if not ocr_dir.exists():  # copy default OCR model only if dir does not exist
         shutil.copytree(
             assets_dir / "MaaCommonAssets" / "OCR" / "ppocr_v5" / "zh_cn",
             ocr_dir,
--- tools/install.py
+++ tools/install.py
@@ -7,15 +7,15 @@ import json
 from configure import configure_ocr_model
 
 
-working_dir = Path(__file__).parent
+working_dir = Path(__file__).parent.parent
 install_path = working_dir / Path("install")
 version = len(sys.argv) > 1 and sys.argv[1] or "v0.0.1"
 
 
 def install_deps():
     if not (working_dir / "deps" / "bin").exists():
-        print("Please download the MaaFramework to \"deps\" first.")
-        print("请先下载 MaaFramework 到 \"deps\"。")
+        print('Please download the MaaFramework to "deps" first.')
+        print('請先下載 MaaFramework 到 "deps"。')
         sys.exit(1)
 
     shutil.copytree(
@@ -69,12 +69,14 @@ def install_chores():
         install_path,
     )
 
+
 def install_agent():
     shutil.copytree(
-        working_dir / "agent",
+        working_dir / "assets" / "agent",
         install_path / "agent",
         dirs_exist_ok=True,
     )
+
 
 if __name__ == "__main__":
     install_deps()
